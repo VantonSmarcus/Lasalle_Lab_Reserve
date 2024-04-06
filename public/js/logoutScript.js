208a035d7cd1a34907a228235e55b7f37877b1fd
IsaacJavid <isaac_javid@dlsu.edu.ph>
2024-04-06 20:53:31 +0800

    make it better

---
+++ public/js/logoutScript.js
@@ -0,0 +1,28 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById("errorMsg").innerText = "";
+    const submitBtn = document.getElementById("submit");
+
+    if (submitBtn) {
+        submitBtn.addEventListener("click", async (e) =>{
+            e.preventDefault();
+
+            const response = await fetch('/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            const response2 = await fetch('/logoutCurrUser', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            window.location.href="/";
+        });
+    }
+    else
+        console.log("loljk");
+});
